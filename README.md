e4575982c343b19f05ad9ae2a27f5cb5c4d392aa
evertonvargas <evertonpassa7@gmail.com>
2021-06-13 15:00:24 -0300

    api rest finalizada

---
+++ README.md
@@ -0,0 +1,51 @@
+## :computer: Criando API REST 
+Esse projeto permitiu entender melhor como funcionam os https verbs e sua associação com o CRUD de um database. Entendi melhor também request e response, assim como, o app.route() do expressjs.
+<br>
+
+## HTTP request methods
+- POST: inserir
+- GET: ler
+- PATCH: atualizar
+- DELETE: deletar
+<br>
+
+## CRUD
+- CREATE
+- READ
+- UPDATE
+- DELETE
+<br>
+
+## Request e Response
+Request é quando eu faço uma requisição(pedido), posso passar valores também.
+Exemplo:
+Tenho essa rota na minha API
+```Javascript
+app.route("/animais")
+.post((req, res) => {
+  const animalCreate = new Animal({
+    name: req.body.animal,
+    lifeMax: req.body.lifeMax,
+  });
+  animalCreate.save()
+    .then(() => {
+      res.send("Cadastro efetuado com sucesso")
+    }).catch((err) => {
+      res.send(err)
+    })
+})
+```
+
+No Postman faço um método *POST* para a url da requisição *localhost:3000/animais* com o body:
+```Json
+{
+    "animal":"Pinguim-Imperador",
+    "lifeMax": 20
+}
+```
+Response é a resposta dada por essa solicitação.
+
+:warning: requisição e solicitação são sinônimos.
+
+
+
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const port = 3000;
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const express = require("express")
+const mongoose = require("mongoose")
+const port = 3000
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 mongoose.connect("mongodb://localhost:27017/animaisDB", {
   useNewUrlParser: true,
@@ -14,37 +14,75 @@ const animalSchema = new mongoose.Schema({
   name: { type: String, required: true },
   lifeMax: Number,
 });
-const Animal = mongoose.model("Animal", animalSchema);
+const Animal = mongoose.model("Animal", animalSchema)
 
-app.get("/animais", (req, res) => {
+app.route("/animais")
+
+.get((req, res) => {
   Animal.find((err, animais) => {
     if (!err) {
-      if (animais.length === 0) {
-        res.send("Nenhum animal cadastrado ainda");
-      } else {
-        res.send(animais);
-      }
+      if (animais.length === 0) res.send("Nenhum animal cadastrado ainda")
+      else res.send(animais)
     } else {
-      console.log(err);
+      console.log(err)
     }
-  });
-});
+  })
+})
 
-app.post("/animais", (req, res) => {
-  console.log(req.body);
+.post((req, res) => {
   const animalCreate = new Animal({
     name: req.body.animal,
     lifeMax: req.body.lifeMax,
   });
   animalCreate.save()
     .then(() => {
-      res.send("Cadastro efetuado com sucesso");
+      res.send("Cadastro efetuado com sucesso")
+    }).catch((err) => {
+      res.send(err)
     })
-    .catch((err) => {
-      res.send(err);
+})
+
+.delete((req, res) => {
+  Animal.deleteMany(function (err) {
+    if (!err) res.send("Todos os animais foram deletados com sucesso")
+    else res.send(err)
+  })
+});
+
+app.route("/animais/:animal")
+.get((req,res)=>{
+  console.log("Get animal")
+  console.log(req.params)
+  Animal.find({name: req.params.animal},(err, result)=>{
+    if(!err){
+      if(result) res.send(result)
+      else res.send("Nenhum animal encontrado")
+    }else{
+      res.send(err)
+    }
+  })
+})
+
+.patch((req,res)=>{
+  Animal.updateOne(
+    {name:req.params.animal},
+    {name:req.body.animal, lifeMax: req.body.lifeMax}, 
+    function(err){
+      if(!err){
+        res.send("Animal atualizado com sucesso")
+      }else{
+        res.send(err)
+      }
     });
+})
+
+.delete((req,res)=>{
+  Animal.deleteOne({name:req.params.animal}, err=>{
+    if(!err)res.send("Animal foi deletado com sucesso")
+    else res.send(err)
+  })
 });
 
 app.listen(port, () => {
-  console.log(`Server running http://localhost:${port}`);
+  console.log(`Server running http://localhost:${port}`)
 });
